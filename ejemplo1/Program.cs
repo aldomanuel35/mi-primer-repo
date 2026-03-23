6d57332d38f951484ecd0f2d2c1631a0d214d4ee
aldoUPN <aldomanuel353@gmail.com>
2026-03-23 16:46:51 -0500

    "prueba"

---
+++ ejemplo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+ 
+
+    static void Main(string[] args) {
+Console.WriteLine("Ingrese N1");
+int a =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese N2");
+int b =Convert.ToInt32(Console.ReadLine());
+int suma=a+b;
+
+Console.WriteLine("la suma es " +suma);
+
+}
+
+}
