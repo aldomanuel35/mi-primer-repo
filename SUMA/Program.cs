b319e5e162adcc4a4f18fa7771f019139071ab61
aldoUPN <aldomanuel353@gmail.com>
2026-03-25 14:16:58 -0500

    Función suma implementada

---
+++ SUMA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class suma
+{
+    public static void Main ()
+    {
+        int x;
+        int y;
+        int suma;
+    Console.WriteLine("ingrese primer numero");
+        x=Convert.ToInt32(Console.ReadLine());
+        
+    Console.WriteLine("ingrese segundo numero");
+        y=Convert.ToInt32(Console.ReadLine());
+suma=x+y;
+    Console.WriteLine("El resultados es "+  suma );
+    }
+        
+}
