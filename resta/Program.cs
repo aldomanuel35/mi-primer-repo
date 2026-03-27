d93884765782553c19859e4caa684296eed50530
aldoUPN <aldomanuel353@gmail.com>
2026-03-27 17:14:40 -0500

    se realizo programa de resta

---
+++ resta/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class resta
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num1 - num2;
+
+        Console.WriteLine("El resultado de la resta es: " + resultado);
+    }
+}
